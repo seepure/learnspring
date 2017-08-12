61d8c74994d7f76ae47a4d198c48697b05bb36e5
qiang.xiao <qiang.xiao@tendcloud.com>
2017-08-13 01:01:34 +0800

    add life cycle of Beans.

--- ch04-lifeCycleOfBeans/src/main/java/edu/seepure/javaee/spring/lifecycle/LifeCycleTest.java
+++ ch04-lifeCycleOfBeans/src/main/java/edu/seepure/javaee/spring/lifecycle/LifeCycleTest.java
@@ -16,7 +16,15 @@ import edu.seepure.javaee.spring.lifecycle.util.SpringUtil;
  *
  * Note:
  * 1) SpringAware interfaces is not recommended because they couples your code with spring which is not the IOC style.
- * 2) BeanPostProcessor interface
+ * 2) BeanPostProcessor interface : BeanPostProcessors operate on bean (or object) instances;
+ *  that is to say, the Spring IoC container instantiates a bean instance and then BeanPostProcessors do their work.
+ *  Classes that implement the BeanPostProcessor interface are special and are treated differently
+ *  by the container. All BeanPostProcessors and beans that they reference directly are
+ *  instantiated on startup, as part of the special startup phase of the ApplicationContext. Next,
+ *  all BeanPostProcessors are registered in a sorted fashion and applied to all further beans in the
+ *  container. Because AOP auto-proxying is implemented as a BeanPostProcessor itself, neither
+ *  BeanPostProcessors nor the beans they reference directly are eligible for auto-proxying, and
+ *  thus do not have aspects woven into them.
  * 3) default init/destroy method in "<beans default-init-method="init" default-destroy-method="destroy" ...\>"
  * 4) self init/destroy method in "<bean init-method="studentInit" />"
  */
